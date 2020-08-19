c258a68a7a5c00e34c2df7d8ff29e4007141fda8
matheus <matheus.xmaz10@gmail.com>
2020-08-19 09:48:31 -0300

    Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Img from "./photo.jpg";
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaTwitter,
+  FaTelegramPlane,
+  FaMediumM,
+} from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <section className="profile">
+      <div className="left">
+        <img src={Img} alt="" />
+      </div>
+      <div className="right">
+        <h1>
+          Matheus <span>Fortunato</span>
+        </h1>
+        <p className="description">
+          Fullstack javascript developer and Mobile.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/matheus-fortunato-14513b195/"
+              target="__blank"
+            >
+              <FaLinkedinIn />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/xmaz10" target="__blank">
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/mfortunato20" target="__blank">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://t.me/mfortunato20" target="__blank">
+              <FaTelegramPlane />
+            </a>
+          </li>
+          <li>
+            <a href="https://medium.com/@matheus.xmaz10" target="__blank">
+              <FaMediumM />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
