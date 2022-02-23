60fac392b7fe2bb916555c30254848d42b95afc0
matheus <matheus.xmaz10@gmail.com>
2022-02-23 09:21:44 -0300

    change constants value

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,5 +2,5 @@ export const TimeLineData = [
   { year: 2016, text: "I started studies in programming" },
   { year: 2017, text: "I started studies in java language" },
   { year: 2019, text: "I chose focused JavaScript" },
-  { year: 2021, text: "Started to produce content for medium and linkedin" },
+  { year: 2021, text: "Started to produce content for dev.to" },
 ];
